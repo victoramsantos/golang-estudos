b5d5f5b142d8d611a0b80918c1bc931533883755
victor.amsantos <victor.amsantos@gmail.com>
2022-01-28 12:11:46 -0300

    added localobservability with prom and grafana

--- saga-pattern/locals/setup.sh
+++ saga-pattern/locals/setup.sh
@@ -2,4 +2,6 @@
 
 docker-compose --file localstack/docker-compose.yml up -d 
 sleep 15
-docker-compose --file localenv/docker-compose.yml up -d 
+docker-compose --file localenv/docker-compose.yml up -d --build
+sleep 15
+docker-compose --file localobservability/docker-compose.yml up -d --build 
