615cc13833b03fee6b67d22348be8e6b356d254e
Victor.Santos <victor.santos@ORA-4360.local>
2022-03-29 18:11:02 -0300

    update down script

--- saga-pattern/locals/down.sh
+++ saga-pattern/locals/down.sh
@@ -2,3 +2,4 @@
 
 docker-compose --file localstack/docker-compose.yml down 
 docker-compose --file localenv/docker-compose.yml down 
+docker-compose --file localobservability/docker-compose.yml down
