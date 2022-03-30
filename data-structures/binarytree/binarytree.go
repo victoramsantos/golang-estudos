4c28572a1d17aff624863f2e13c5d43caafcc675
Victor.Santos <victor.santos@ORA-4360.local>
2022-03-30 17:57:26 -0300

    added binary tree struct and methods

--- data-structures/binarytree/binarytree.go
+++ data-structures/binarytree/binarytree.go
@@ -1,5 +1,7 @@
 package binarytree
 
+import "fmt"
+
 type BinaryTree struct {
 	left  *BinaryTree
 	right *BinaryTree
@@ -13,29 +15,108 @@ func NewBinaryTree(value int) *BinaryTree {
 		value: value,
 	}
 }
+
 func (tree *BinaryTree) Add(value int) {
-	var pointer *BinaryTree
+	var pointer **BinaryTree
 
 	if value > tree.value {
-		pointer = tree.right
+		pointer = &tree.right
 	} else {
-		pointer = tree.left
+		pointer = &tree.left
 	}
 
-	if pointer == nil {
-		pointer = &BinaryTree{
+	if *pointer == nil {
+		*pointer = &BinaryTree{
 			left:  nil,
 			right: nil,
 			value: value,
 		}
 	} else {
-		pointer.Add(value)
+		(*pointer).Add(value)
 	}
 }
 
+func (tree *BinaryTree) ToString() {
+	tree._toString("")
+}
+
+func (tree *BinaryTree) _toString(line string) {
+	fmt.Printf("%d\n", tree.value)
+
+	if tree.left != nil {
+		fmt.Printf("%s|__", line)
+		tree.left._toString(fmt.Sprintf("  %s", line))
+	}
+
+	if tree.right != nil {
+		fmt.Printf("%s|__", line)
+		tree.right._toString(fmt.Sprintf("  %s", line))
+	}
+}
+
+func (tree *BinaryTree) CountMembers() int {
+	sum := 0
+	if tree.left != nil {
+		sum += tree.left.CountMembers()
+	}
+	if tree.right != nil {
+		sum += tree.right.CountMembers()
+	}
+	return sum + 1
+}
+
+func (tree *BinaryTree) Min() int {
+	min := tree.value
+	tree._min(&min)
+	return min
+}
+
+func (tree *BinaryTree) _min(value *int) {
+	if tree.value < *value {
+		*value = tree.value
+	}
+	if tree.left != nil {
+		tree.left._min(value)
+	}
+	if tree.right != nil {
+		tree.right._min(value)
+	}
+}
+
+func (tree *BinaryTree) Max() int {
+	max := tree.value
+	tree._max(&max)
+	return max
+}
+
+func (tree *BinaryTree) _max(value *int) {
+	if tree.value > *value {
+		*value = tree.value
+	}
+	if tree.left != nil {
+		tree.left._max(value)
+	}
+	if tree.right != nil {
+		tree.right._max(value)
+	}
+}
+
+func (tree *BinaryTree) HasValue(value int) bool {
+	if tree.value == value {
+		return true
+	} else {
+		if value > tree.value {
+			if tree.right != nil {
+				return tree.right.HasValue(value)
+			}
+		} else {
+			if tree.left != nil {
+				return tree.left.HasValue(value)
+			}
+		}
+	}
+	return false
+}
+
 // HasValue
-// ToString
 // Height
-// Min
-// Max
-// Sum
--- data-structures/binarytree/binarytree_test.go
+++ data-structures/binarytree/binarytree_test.go
@@ -1,1 +1,79 @@
 package binarytree_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/victoramsantos/golang-estudos/data-structures/binarytree"
+)
+
+func TestBinaryTree(t *testing.T) {
+
+	t.Run("printing values", func(t *testing.T) {
+		values := [6]int{5, 3, 6, 1, 2, 4}
+		tree := binarytree.NewBinaryTree(values[0])
+
+		for i := 1; i < len(values); i++ {
+			tree.Add(values[i])
+		}
+		tree.ToString()
+	})
+
+	t.Run("adding values", func(t *testing.T) {
+		values := [6]int{5, 3, 6, 1, 2, 4}
+		tree := binarytree.NewBinaryTree(values[0])
+
+		for i := 1; i < len(values); i++ {
+			tree.Add(values[i])
+		}
+
+		assert.Equal(t, len(values), tree.CountMembers())
+	})
+
+	t.Run("minimum value", func(t *testing.T) {
+		values := [6]int{5, 3, 6, 1, 2, 4}
+		tree := binarytree.NewBinaryTree(values[0])
+
+		for i := 1; i < len(values); i++ {
+			tree.Add(values[i])
+		}
+
+		assert.Equal(t, len(values), tree.CountMembers())
+		assert.Equal(t, 1, tree.Min())
+	})
+
+	t.Run("maximum value", func(t *testing.T) {
+		values := [6]int{5, 3, 6, 1, 2, 4}
+		tree := binarytree.NewBinaryTree(values[0])
+
+		for i := 1; i < len(values); i++ {
+			tree.Add(values[i])
+		}
+
+		assert.Equal(t, len(values), tree.CountMembers())
+		assert.Equal(t, 6, tree.Max())
+	})
+
+	t.Run("has value", func(t *testing.T) {
+		values := [6]int{5, 3, 7, 1, 2, 4}
+		tree := binarytree.NewBinaryTree(values[0])
+
+		for i := 1; i < len(values); i++ {
+			tree.Add(values[i])
+		}
+
+		assert.Equal(t, len(values), tree.CountMembers())
+		assert.True(t, tree.HasValue(1))
+		assert.True(t, tree.HasValue(2))
+		assert.True(t, tree.HasValue(3))
+		assert.True(t, tree.HasValue(4))
+		assert.True(t, tree.HasValue(5))
+		assert.True(t, tree.HasValue(7))
+
+		assert.False(t, tree.HasValue(0))
+		assert.False(t, tree.HasValue(6))
+		assert.False(t, tree.HasValue(8))
+
+	})
+
+}
