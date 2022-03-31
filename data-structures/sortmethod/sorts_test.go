1328d99a7b114f16c81a659d0f73bfcbaff6b539
Victor.Santos <victor.amsantos@gmail.com>
2022-03-31 13:47:20 -0300

    uncomment tests

--- data-structures/sortmethod/sorts_test.go
+++ data-structures/sortmethod/sorts_test.go
@@ -42,16 +42,16 @@ func TestSort(t *testing.T) {
 		}
 	})
 
-	// t.Run("quicksort - sorted", func(t *testing.T) {
-	// 	array := []int{1, 2, 3, 4, 5, 6, 7, 8}
-	// 	sortedArray := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	t.Run("quicksort - sorted", func(t *testing.T) {
+		array := []int{1, 2, 3, 4, 5, 6, 7, 8}
+		sortedArray := []int{1, 2, 3, 4, 5, 6, 7, 8}
 
-	// 	sortmethod.QuickSort(array)
+		sortmethod.QuickSort(array)
 
-	// 	for i := 0; i < len(array); i++ {
-	// 		assert.Equal(t, array[i], sortedArray[i])
-	// 	}
-	// })
+		for i := 0; i < len(array); i++ {
+			assert.Equal(t, array[i], sortedArray[i])
+		}
+	})
 
 	t.Run("quicksort - unsorted", func(t *testing.T) {
 		array := []int{6, 4, 5, 2, 1, 3, 7, 8}
@@ -64,15 +64,14 @@ func TestSort(t *testing.T) {
 		}
 	})
 
-	// t.Run("quicksort - desc sorted", func(t *testing.T) {
-	// 	array := []int{8, 7, 6, 5, 4, 3, 2, 1}
-	// 	sortedArray := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	t.Run("quicksort - desc sorted", func(t *testing.T) {
+		array := []int{8, 7, 6, 5, 4, 3, 2, 1}
+		sortedArray := []int{1, 2, 3, 4, 5, 6, 7, 8}
 
-	// 	sortmethod.QuickSort(array)
+		sortmethod.QuickSort(array)
 
-	// 	for i := 0; i < len(array); i++ {
-	// 		assert.Equal(t, array[i], sortedArray[i])
-	// 	}
-	// })
-
+		for i := 0; i < len(array); i++ {
+			assert.Equal(t, array[i], sortedArray[i])
+		}
+	})
 }
